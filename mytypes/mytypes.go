b2c75ba2b25a28e4aa52b7e87328fe3f0f981537
ThadCastl3 <elginkt@g.cofc.edu>
2024-05-26 14:50:23 -0400

    added some types, based on primitive types, so that I can assign methods for receivers

--- mytypes/mytypes.go
+++ mytypes/mytypes.go
@@ -7,3 +7,9 @@ type MyInt int
 func (i MyInt) Twice() MyInt {
 	return i * 2
 }
+
+type MyString string
+
+func (s MyString) Len() int {
+	return len(s)
+}
--- mytypes/mytypes_test.go
+++ mytypes/mytypes_test.go
@@ -14,3 +14,13 @@ func TestTwice(t *testing.T) {
 		t.Errorf("twice %d: want %d, got %d", input, want, got)
 	}
 }
+
+func TestMyStringLen(t *testing.T) {
+	t.Parallel()
+	input := mytypes.MyString("Hello, Gophers!")
+	want := 15
+	got := input.Len()
+	if want != got {
+		t.Errorf("len(%s): want %d, got %d", input, want, got)
+	}
+}
