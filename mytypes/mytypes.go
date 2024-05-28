90cf4b32c02a7d7e1bf229ef0e5165643559e1c7
ThadCastl3 <elginkt@g.cofc.edu>
2024-05-27 22:43:55 -0400

    working on pointers

--- mytypes/mytypes.go
+++ mytypes/mytypes.go
@@ -1,5 +1,7 @@
 package mytypes
 
+import "strings"
+
 // Twice multiplies its receiver by 2 and returns the result
 
 type MyInt int
@@ -12,4 +14,40 @@ type MyString string
 
 func (s MyString) Len() int {
 	return len(s)
+}
+
+// type MyBuilder strings.Builder
+
+func (mb MyBuilder) Hello() string {
+	return "Hello, Gophers!"
+}
+
+type MyBuilder struct {
+	Contents strings.Builder
+}
+
+type StringUppercaser struct {
+	Contents strings.Builder
+}
+
+func (su StringUppercaser) ToUpper() string {
+	return strings.ToUpper(su.Contents.String())
+}
+
+// func Double(input int) {
+// 	input *= 2
+// }
+
+// modified func Double to use the int pointer *int
+// func Double(input *int) {
+// 	input *= 2
+// }
+
+//fixed the mismatch types
+func Double(input *int) {
+	*input *= 2
+}
+
+func (input *MyInt) Double() {
+	*input *= 2
 }
--- mytypes/mytypes_test.go
+++ mytypes/mytypes_test.go
@@ -3,6 +3,7 @@ package mytypes_test
 import (
 	"mytypes"
 	"testing"
+	"strings"
 )
 
 func TestTwice(t *testing.T) {
@@ -24,3 +25,92 @@ func TestMyStringLen(t *testing.T) {
 		t.Errorf("len(%s): want %d, got %d", input, want, got)
 	}
 }
+
+func TestStringBuilder(t *testing.T) {
+	t.Parallel()
+	var sb strings.Builder
+	sb.WriteString("Hello, ")
+	sb.WriteString("Gophers!")
+	want := "Hello, Gophers!"
+	got := sb.String()
+	if want != got {
+		t.Errorf("got %q, want %q", got, want)
+	}
+	wantlen := 15
+	gotlen := sb.Len()
+	if wantlen != gotlen {
+		t.Errorf("%q: want len %d, got %d", sb.String(), wantlen, gotlen)
+	}
+}
+
+// func TestMyBuilderlen(t *testing.T) {
+// 	t.Parallel()
+// 	var mb mytypes.MyBuilder
+// 	want := 15
+// 	got := mb.Len()
+// 	if want != got {
+// 		t.Errorf("want %d, got %d", want, got)
+// 	}
+// }
+
+// func TestMyBuilderHello(t *testing.T) {
+// 	t.Parallel()
+// 	var mb mytypes.MyBuilder
+// 	want := "Hello, Gophers!"
+// 	got := mb.Hello()
+// 	if want != got {
+// 		t.Errorf("want %q, got %q", want, got)
+// 	}
+// }
+
+func TestMyBuilder(t *testing.T) {
+	t.Parallel()
+	var mb mytypes.MyBuilder
+	mb.Contents.WriteString("Hello, ")
+	mb.Contents.WriteString("Gophers!")
+	want := "Hello, Gophers!"
+	got := mb.Contents.String()
+	if want != got {
+		t.Errorf("got %q, want %q", got, want)
+	}
+	wantlen := 15
+	gotlen := mb.Contents.Len()
+	if wantlen != gotlen {
+		t.Errorf("%q: want len %d, got %d", mb.Contents.String(), wantlen, gotlen)
+	}
+}
+
+func TestStringUppercaser(t *testing.T) {
+	t.Parallel()
+	var su mytypes.StringUppercaser
+	su.Contents.WriteString("Hello, Gophers!")
+	want := "HELLO, GOPHERS!"
+	got := su.ToUpper()
+	if want != got {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+// func TestDouble(t *testing.T) {
+// 	t.Parallel()
+// 	var x int = 12
+// 	want := 24
+// 	mytypes.Double(x)
+// 	if want != x {
+// 		t.Errorf("want %d, got %d", want, x)
+// 	}
+// }
+
+// modified this test to use a pointer and a sharing operator
+// func TestDouble(t *testing.T) {
+// 	t.Parallel()
+// 	var x int = 12
+// 	want := 24
+// 	mytypes.Double(&x)
+// 	if want != x {
+// 		t.Errorf("want %d, got %d", want, x)
+// 	}
+// }
+
+// modifying test to use a pointer for the MyInt type
+//
